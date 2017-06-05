71018183d199c3c803b8b132a820379560cc716e
Jeffrey C. Ollie <jeff@ocjtech.us>
2017-06-05 14:12:13 -0500

    wip

--- starpy/manager.py
+++ starpy/manager.py
@@ -1136,7 +1136,7 @@ class AMIFactory(Factory):
 
     def buildProtocol(self, addr):
         self.log.debug('Building AMI protocol for {addr:}', addr = addr)
-        return AMIProtocol(self, self.log_lines_sent, self.log_lines_received, self.log_messages_sent, self.log_messages_recived)
+        return AMIProtocol(self, log_lines_sent = self.log_lines_sent, log_lines_received = self.log_lines_received, log_messages_sent = self.log_messages_sent, log_messages_received = self.log_messages_recived)
 
 class AMIService(object):
     def __init__(self, reactor, username, secret,
